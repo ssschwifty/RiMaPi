d819114e0a5bb030cedc364df27b4a23d4ffe3ac
Sarah Wöckener <ssschwifty@outlook.com>
2016-04-30 14:21:20 +0200

    change viewtransition method

--- app/scripts/controller/mainController.js
+++ app/scripts/controller/mainController.js
@@ -2,14 +2,25 @@ angular.module('riot.controller')
 .controller('MainController', ['$scope', '$state', 'stringConstants', function($scope, $state, stringConstants) {
 
 	function initiate() {
-		$state.go('home');
 		$scope.btnGrade = stringConstants.btnGrade;
 		$scope.btnLoot = stringConstants.btnLoot;
 		$scope.btnScore = stringConstants.btnScore;
 		$scope.btnDocu = stringConstants.btnDocu;
+		$state.go('home');
 	}
+	
 	initiate();
-	$scope.openView= function(view) {
-		$state.go(view);
+
+	$scope.openGradeView = function() {
+		$state.go('grade');
+	}
+	$scope.openLootView = function() {
+		$state.go('loot');
+	}
+	$scope.openScoreView = function() {
+		$state.go('score');
+	}
+	$scope.openDocuView = function() {
+		$state.go('docu');
 	}
 }]);
