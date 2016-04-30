88b2b5d427f5e647275f4aa7feb45fcc1661ca30
Sarah Wöckener <ssschwifty@outlook.com>
2016-04-30 13:46:03 +0200

    add navigation buttons and transitionmethod

--- app/scripts/controller/mainController.js
+++ app/scripts/controller/mainController.js
@@ -1,4 +1,11 @@
-angular.module('riot')
-.controller('MainController', function() {
-	
-});
+angular.module('riot.controller')
+.controller('MainController', ['$scope', '$state', 'stringConstants', function($scope, $state, stringConstants) {
+
+	function initiate() {
+		$state.go('home');
+	}
+	initiate();
+	$scope.openView= function(view) {
+		$state.go(view);
+	}
+}]);
