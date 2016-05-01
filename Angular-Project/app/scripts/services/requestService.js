f6c6105db7684097a00f2f7065cebf6155d0e9a3
Sarah Wöckener <ssschwifty@outlook.com>
2016-05-01 23:31:50 +0200

    build first request

--- Angular-Project/app/scripts/services/requestService.js
+++ Angular-Project/app/scripts/services/requestService.js
@@ -1,4 +1,15 @@
 angular.module('riot.services')
 .factory('RequestService', function($http) {
 
+	var championMasteries;
+
+	return {
+		getMockupData: function() {
+			 return $http.get('../app/scripts/controller/narmor_test_result.json');
+		},
+		getAllChampionMasteries: function(playerId) {
+			// TODO reeuest zu eigenem server
+		}
+	}
+
 });
--- Angular-Project/app/scripts/services/sharedProperties.js
+++ Angular-Project/app/scripts/services/sharedProperties.js
@@ -1,4 +1,17 @@
 angular.module('riot.services')
 .factory('SharedProperties', function(RequestService, Mapping) {
+
+	var championId;
+	var playerId;
+
+	return {
+		getAllChampionMasteries: function() {
+			RequestService.getAllChampionMasteries(playerId);
+		},
+
+		getMockupData: function() {
+			return RequestService.getMockupData();
+		}
+	}
 	
 });
