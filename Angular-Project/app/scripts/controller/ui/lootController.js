81d1304bbcfb18e8d0f18687af95852245612caa
Sarah Wöckener <ssschwifty@outlook.com>
2016-05-01 23:32:14 +0200

    add minimal example for loot screen

--- Angular-Project/app/scripts/controller/ui/lootController.js
+++ Angular-Project/app/scripts/controller/ui/lootController.js
@@ -1,6 +1,31 @@
 angular.module('riot.controller.ui')
 .controller('LootController', function($scope, SharedProperties) {
 
+	$scope.grantedChests = "";
+	$scope.notGranted = "";
+	$scope.potential;
+
+	$scope.getData = function() {
+		SharedProperties.getMockupData()
+		.then(function(response) {
+			var testresult = response.data;
+			var granted = 0;
+			var notGranted = 0;
+			var potential = [];
+			for (var i = 0; i < testresult.length; i++) {
+				if(testresult[i].chestGranted) {
+					granted++;
+				} else {
+					potential.push(testresult[i]);
+					notGranted++;
+				}
+			}
+			console.log(potential);
+			$scope.grantedChests = granted.toString();
+			$scope.notGranted = notGranted.toString();
+			$scope.potential = potential;
+		});
+	}
 
 });
 
