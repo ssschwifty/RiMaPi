2b493708a8ad38a20f4449ce1973a3ea0fca9e57
Sarah Wöckener <ssschwifty@outlook.com>
2016-05-04 23:36:02 +0200

    change region mapping

--- Angular-Project/app/scripts/constants/mapping.js
+++ Angular-Project/app/scripts/constants/mapping.js
@@ -1,22 +1,18 @@
 angular.module('riot')
 .constant('Mapping', {
 
-	"grades": {
-		"id": "S+", value: 14,
-		"id": "S", value: 13,
-		"id": "S-", value: 12,
-		"id": "A+", value: 11,
-		"id": "A", value: 10,
-		"id": "A-", value: 9,
-		"id": "B+", value: 8,
-		"id": "B", value: 7,
-		"id": "B-", value: 6,
-		"id": "C+", value: 5,
-		"id": "C", value: 4,
-		"id": "C-", value: 3,
-		"id": "D+", value: 2,
-		"id": "D", value: 1,
-		"id": "D-", value: 0
+	"regions": {
+		"br": "Brazil",
+		"eun": "EU Nordic & East",
+		"euw": "EU West",
+		"la": "Latin America North",
+		"la": "Latin America South",
+		"na": "North America",
+		"oc": "Oceania",
+		"ru": "Russia",
+		"tr": "Turkey",
+		"jp": "Japan",
+		"kr": "Republic of Korea"
 	},
 
 	"champions": {
