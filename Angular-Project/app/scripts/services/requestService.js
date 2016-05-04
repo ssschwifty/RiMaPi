39fcddc7ffb187071201a1a6ff090386631acaf8
Sarah Wöckener <ssschwifty@outlook.com>
2016-05-04 23:39:44 +0200

    add base url for apirequests, add getContinent Method

--- Angular-Project/app/scripts/services/requestService.js
+++ Angular-Project/app/scripts/services/requestService.js
@@ -2,13 +2,20 @@ angular.module('riot.services')
 .factory('RequestService', function($http) {
 
 	var championMasteries;
+	var baseUrl = 'http://v22016053572334167.supersrv.de:3002/';
+	var continent;
+	function _getContinent(response) {
+		return $http.get('test');
+	}
 
 	return {
-		getMockupData: function() {
-			 return $http.get('../app/scripts/controller/narmor_test_result.json');
+		getAllChampionMasteries: function(region, summoner) {
+			return $http.get(baseUrl + 'GetAllChampionMasteries/p/' + region + '/u/' + summoner);
 		},
-		getAllChampionMasteries: function(playerId) {
-			// TODO reeuest zu eigenem server
+		// if no region was found via geolocation it returns null, else it returns the region id
+		getContinent: function(response) {
+			navigator.geolocation.getCurrentPosition(_getContinent);
+			console.log(response);
 		}
 	}
 
