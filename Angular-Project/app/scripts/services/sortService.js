373ea811ecd95cd4433355cd1578f05228c9c70a
Sarah Wöckener <ssschwifty@outlook.com>
2016-05-04 17:30:27 +0200

    added sortservice

---
+++ Angular-Project/app/scripts/services/sortService.js
@@ -0,0 +1,32 @@
+angular.module('riot.services')
+.service('Sort', function() {
+
+	var gradeOrder = [
+		"S+", "S", "S-",
+		"A+", "A", "A-",
+		"B+", "B", "B-",
+		"C+", "C", "C-",
+		"D+", "D", "D-",
+		undefined
+	];
+
+	sortFunction = function(a, b) {
+		if(gradeOrder.indexOf(a.highestGrade) - gradeOrder.indexOf(b.highestGrade) == 0) {
+			return b.championPoints - a.championPoints;
+		} else {
+			return gradeOrder.indexOf(a.highestGrade) - gradeOrder.indexOf(b.highestGrade);
+		}
+	}
+	
+	return {
+		sortByGradeThenChampionPoints: function(champions) {
+			champions.sort(sortFunction);
+			var results = champions.map(function(a) {
+				[a.championId];
+			});
+			console.log(results);
+			return results;
+		}
+
+	}
+});
