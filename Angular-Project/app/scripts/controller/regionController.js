8843f18b8d5ce04d79d9bbb6ea0e673457f06848
Sarah Wöckener <ssschwifty@outlook.com>
2016-05-04 23:42:16 +0200

    initiate region / select region

--- Angular-Project/app/scripts/controller/regionController.js
+++ Angular-Project/app/scripts/controller/regionController.js
@@ -1,7 +1,29 @@
 angular.module('riot.controller')
 .controller('RegionController', function($scope, SharedProperties) {
-	$scope.activeRegion = $scope.regions[0];
-	$scope.regionSelected = function(region) {
-		$scope.activeRegion = region;
+	
+	$scope.$on('$stateChangeSuccess', function() {
+		// needs to be handled differently!
+		defineRegion();
+	});
+
+	$scope.regions = SharedProperties.getRegions();
+
+	function defineRegion() {		
+		var activeRId = SharedProperties.getActiveRegionId();
+		if(activeRId != undefined && activeRId != null) {
+			//debugging
+			//$scope.activeRegionId = 'euw';
+			var gpsRegion = SharedProperties.getContinent();
+			$scope.activeRegionId = $scope.regions[gpsRegion];
+		} else {
+			$scope.activeRegionId = 'euw';
+		}
+		SharedProperties.setActiveRegionId($scope.activeRegionId);
 	}
+
+	$scope.regionSelected = function(_regionId) {
+		$scope.activeRegionId = _regionId;
+		SharedProperties.setActiveRegionId(_regionId);
+	}
+
 });
