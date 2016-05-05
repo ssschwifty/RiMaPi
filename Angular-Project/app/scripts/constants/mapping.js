45774e1b693e4f15276a2aa659f28abea727b6c3
Sarah Wöckener <ssschwifty@outlook.com>
2016-05-05 20:46:09 +0200

    add displaynames to champion mapping

--- Angular-Project/app/scripts/constants/mapping.js
+++ Angular-Project/app/scripts/constants/mapping.js
@@ -17,524 +17,524 @@ angular.module('riot')
 
 	"champions": {
 		"35": {
-			"id": "Shaco",
-			"displayName": "Shaco"
-		}
+			id: "Shaco",
+			displayName: "Shaco"
+		},
 		"36": {
-			"id": "DrMundo",
-			"displayName": "DrMundo"
-		}
+			id: "DrMundo",
+			displayName: "Dr. Mundo"
+		},
 		"33": {
-			"id": "Rammus",
-			"displayName": "Rammus"
-		}
+			id: "Rammus",
+			displayName: "Rammus"
+		},
 		"34": {
-			"id": "Anivia",
-			"displayName": "Anivia"
-		}
+			id: "Anivia",
+			displayName: "Anivia"
+		},
 		"39": {
-			"id": "Irelia",
-			"displayName": "Irelia"
-		}
+			id: "Irelia",
+			displayName: "Irelia"
+		},
 		"157": {
-			"id": "Yasuo",
-			"displayName": "Yasuo"
-		}
+			id: "Yasuo",
+			displayName: "Yasuo"
+		},
 		"37": {
-			"id": "Sona",
-			"displayName": "Sona"
-		}
+			id: "Sona",
+			displayName: "Sona"
+		},
 		"38": {
-			"id": "Kassadin",
-			"displayName": "Kassadin"
-		}
+			id: "Kassadin",
+			displayName: "Kassadin"
+		},
 		"154": {
-			"id": "Zac",
-			"displayName": "Zac"
-		}
+			id: "Zac",
+			displayName: "Zac"
+		},
 		"150": {
-			"id": "Gnar",
-			"displayName": "Gnar"
-		}
+			id: "Gnar",
+			displayName: "Gnar"
+		},
 		"43": {
-			"id": "Karma",
-			"displayName": "Karma"
-		}
+			id: "Karma",
+			displayName: "Karma"
+		},
 		"42": {
-			"id": "Corki",
-			"displayName": "Corki"
-		}
+			id: "Corki",
+			displayName: "Corki"
+		},
 		"41": {
-			"id": "Gangplank",
-			"displayName": "Gangplank"
-		}
+			id: "Gangplank",
+			displayName: "Gangplank"
+		},
 		"40": {
-			"id": "Janna",
-			"displayName": "Janna"
-		}
+			id: "Janna",
+			displayName: "Janna"
+		},
 		"202": {
-			"id": "Jhin",
-			"displayName": "Jhin"
-		}
+			id: "Jhin",
+			displayName: "Jhin"
+		},
 		"203": {
-			"id": "Kindred",
-			"displayName": "Kindred"
-		}
+			id: "Kindred",
+			displayName: "Kindred"
+		},
 		"201": {
-			"id": "Braum",
-			"displayName": "Braum"
-		}
+			id: "Braum",
+			displayName: "Braum"
+		},
 		"22": {
-			"id": "Ashe",
-			"displayName": "Ashe"
-		}
+			id: "Ashe",
+			displayName: "Ashe"
+		},
 		"23": {
-			"id": "Tryndamere",
-			"displayName": "Tryndamere"
-		}
+			id: "Tryndamere",
+			displayName: "Tryndamere"
+		},
 		"24": {
-			"id": "Jax",
-			"displayName": "Jax"
-		}
+			id: "Jax",
+			displayName: "Jax"
+		},
 		"25": {
-			"id": "Morgana",
-			"displayName": "Morgana"
-		}
+			id: "Morgana",
+			displayName: "Morgana"
+		},
 		"26": {
-			"id": "Zilean",
-			"displayName": "Zilean"
-		}
+			id: "Zilean",
+			displayName: "Zilean"
+		},
 		"27": {
-			"id": "Singed",
-			"displayName": "Singed"
-		}
+			id: "Singed",
+			displayName: "Singed"
+		},
 		"28": {
-			"id": "Evelynn",
-			"displayName": "Evelynn"
-		}
+			id: "Evelynn",
+			displayName: "Evelynn"
+		},
 		"29": {
-			"id": "Twitch",
-			"displayName": "Twitch"
-		}
+			id: "Twitch",
+			displayName: "Twitch"
+		},
 		"3": {
-			"id": "Galio",
-			"displayName": "Galio"
-		}
+			id: "Galio",
+			displayName: "Galio"
+		},
 		"161": {
-			"id": "Velkoz",
-			"displayName": "Velkoz"
-		}
+			id: "Velkoz",
+			displayName: "Vel'Koz"
+		},
 		"2": {
-			"id": "Olaf",
-			"displayName": "Olaf"
-		}
+			id: "Olaf",
+			displayName: "Olaf"
+		},
 		"1": {
-			"id": "Annie",
-			"displayName": "Annie"
-		}
+			id: "Annie",
+			displayName: "Annie"
+		},
 		"30": {
-			"id": "Karthus",
-			"displayName": "Karthus"
-		}
+			id: "Karthus",
+			displayName: "Karthus"
+		},
 		"7": {
-			"id": "Leblanc",
-			"displayName": "Leblanc"
-		}
+			id: "Leblanc",
+			displayName: "LeBlanc"
+		},
 		"6": {
-			"id": "Urgot",
-			"displayName": "Urgot"
-		}
+			id: "Urgot",
+			displayName: "Urgot"
+		},
 		"32": {
-			"id": "Amumu",
-			"displayName": "Amumu"
-		}
+			id: "Amumu",
+			displayName: "Amumu"
+		},
 		"5": {
-			"id": "XinZhao",
-			"displayName": "XinZhao"
-		}
+			id: "XinZhao",
+			displayName: "Xin Zhao"
+		},
 		"31": {
-			"id": "Chogath",
-			"displayName": "Chogath"
-		}
+			id: "Chogath",
+			displayName: "Cho'Gath"
+		},
 		"4": {
-			"id": "TwistedFate",
-			"displayName": "TwistedFate"
-		}
+			id: "TwistedFate",
+			displayName: "Twisted Fate"
+		},
 		"9": {
-			"id": "FiddleSticks",
-			"displayName": "FiddleSticks"
-		}
+			id: "FiddleSticks",
+			displayName: "Fiddlesticks"
+		},
 		"8": {
-			"id": "Vladimir",
-			"displayName": "Vladimir"
-		}
+			id: "Vladimir",
+			displayName: "Vladimir"
+		},
 		"19": {
-			"id": "Warwick",
-			"displayName": "Warwick"
-		}
+			id: "Warwick",
+			displayName: "Warwick"
+		},
 		"17": {
-			"id": "Teemo",
-			"displayName": "Teemo"
-		}
+			id: "Teemo",
+			displayName: "Teemo"
+		},
 		"18": {
-			"id": "Tristana",
-			"displayName": "Tristana"
-		}
+			id: "Tristana",
+			displayName: "Tristana"
+		},
 		"15": {
-			"id": "Sivir",
-			"displayName": "Sivir"
-		}
+			id: "Sivir",
+			displayName: "Sivir"
+		},
 		"16": {
-			"id": "Soraka",
-			"displayName": "Soraka"
-		}
+			id: "Soraka",
+			displayName: "Soraka"
+		},
 		"13": {
-			"id": "Ryze",
-			"displayName": "Ryze"
-		}
+			id: "Ryze",
+			displayName: "Ryze"
+		},
 		"14": {
-			"id": "Sion",
-			"displayName": "Sion"
-		}
+			id: "Sion",
+			displayName: "Sion"
+		},
 		"11": {
-			"id": "MasterYi",
-			"displayName": "MasterYi"
-		}
+			id: "MasterYi",
+			displayName: "Master Yi"
+		},
 		"12": {
-			"id": "Alistar",
-			"displayName": "Alistar"
-		}
+			id: "Alistar",
+			displayName: "Alistar"
+		},
 		"21": {
-			"id": "MissFortune",
-			"displayName": "MissFortune"
-		}
+			id: "MissFortune",
+			displayName: "Miss Fortune"
+		},
 		"20": {
-			"id": "Nunu",
-			"displayName": "Nunu"
-		}
+			id: "Nunu",
+			displayName: "Nunu"
+		},
 		"107": {
-			"id": "Rengar",
-			"displayName": "Rengar"
-		}
+			id: "Rengar",
+			displayName: "Rengar"
+		},
 		"106": {
-			"id": "Volibear",
-			"displayName": "Volibear"
-		}
+			id: "Volibear",
+			displayName: "Volibear"
+		},
 		"105": {
-			"id": "Fizz",
-			"displayName": "Fizz"
-		}
+			id: "Fizz",
+			displayName: "Fizz"
+		},
 		"104": {
-			"id": "Graves",
-			"displayName": "Graves"
-		}
+			id: "Graves",
+			displayName: "Graves"
+		},
 		"103": {
-			"id": "Ahri",
-			"displayName": "Ahri"
-		}
+			id: "Ahri",
+			displayName: "Ahri"
+		},
 		"102": {
-			"id": "Shyvana",
-			"displayName": "Shyvana"
-		}
+			id: "Shyvana",
+			displayName: "Shyvana"
+		},
 		"99": {
-			"id": "Lux",
-			"displayName": "Lux"
-		}
+			id: "Lux",
+			displayName: "Lux"
+		},
 		"101": {
-			"id": "Xerath",
-			"displayName": "Xerath"
-		}
+			id: "Xerath",
+			displayName: "Xerath"
+		},
 		"412": {
-			"id": "Thresh",
-			"displayName": "Thresh"
-		}
+			id: "Thresh",
+			displayName: "Thresh"
+		},
 		"98": {
-			"id": "Shen",
-			"displayName": "Shen"
-		}
+			id: "Shen",
+			displayName: "Shen"
+		},
 		"96": {
-			"id": "KogMaw",
-			"displayName": "KogMaw"
-		}
+			id: "KogMaw",
+			displayName: "Kog'Maw"
+		},
 		"222": {
-			"id": "Jinx",
-			"displayName": "Jinx"
-		}
+			id: "Jinx",
+			displayName: "Jinx"
+		},
 		"223": {
-			"id": "TahmKench",
-			"displayName": "TahmKench"
-		}
+			id: "TahmKench",
+			displayName: "Tahm Kench"
+		},
 		"92": {
-			"id": "Riven",
-			"displayName": "Riven"
-		}
+			id: "Riven",
+			displayName: "Riven"
+		},
 		"91": {
-			"id": "Talon",
-			"displayName": "Talon"
-		}
+			id: "Talon",
+			displayName: "Talon"
+		},
 		"90": {
-			"id": "Malzahar",
-			"displayName": "Malzahar"
-		}
+			id: "Malzahar",
+			displayName: "Malzahar"
+		},
 		"10": {
-			"id": "Kayle",
-			"displayName": "Kayle"
-		}
+			id: "Kayle",
+			displayName: "Kayle"
+		},
 		"429": {
-			"id": "Kalista",
-			"displayName": "Kalista"
-		}
+			id: "Kalista",
+			displayName: "Kalista"
+		},
 		"421": {
-			"id": "RekSai",
-			"displayName": "RekSai"
-		}
+			id: "RekSai",
+			displayName: "Rek'Sai"
+		},
 		"420": {
-			"id": "Illaoi",
-			"displayName": "Illaoi"
-		}
+			id: "Illaoi",
+			displayName: "Illaoi"
+		},
 		"89": {
-			"id": "Leona",
-			"displayName": "Leona"
-		}
+			id: "Leona",
+			displayName: "Leona"
+		},
 		"117": {
-			"id": "Lulu",
-			"displayName": "Lulu"
-		}
+			id: "Lulu",
+			displayName: "Lulu"
+		},
 		"79": {
-			"id": "Gragas",
-			"displayName": "Gragas"
-		}
+			id: "Gragas",
+			displayName: "Gragas"
+		},
 		"78": {
-			"id": "Poppy",
-			"displayName": "Poppy"
-		}
+			id: "Poppy",
+			displayName: "Poppy"
+		},
 		"114": {
-			"id": "Fiora",
-			"displayName": "Fiora"
-		}
+			id: "Fiora",
+			displayName: "Fiora"
+		},
 		"115": {
-			"id": "Ziggs",
-			"displayName": "Ziggs"
-		}
+			id: "Ziggs",
+			displayName: "Ziggs"
+		},
 		"77": {
-			"id": "Udyr",
-			"displayName": "Udyr"
-		}
+			id: "Udyr",
+			displayName: "Udyr"
+		},
 		"112": {
-			"id": "Viktor",
-			"displayName": "Viktor"
-		}
+			id: "Viktor",
+			displayName: "Viktor"
+		},
 		"113": {
-			"id": "Sejuani",
-			"displayName": "Sejuani"
-		}
+			id: "Sejuani",
+			displayName: "Sejuani"
+		},
 		"110": {
-			"id": "Varus",
-			"displayName": "Varus"
-		}
+			id: "Varus",
+			displayName: "Varus"
+		},
 		"111": {
-			"id": "Nautilus",
-			"displayName": "Nautilus"
-		}
+			id: "Nautilus",
+			displayName: "Nautilus"
+		},
 		"119": {
-			"id": "Draven",
-			"displayName": "Draven"
-		}
+			id: "Draven",
+			displayName: "Draven"
+		},
 		"432": {
-			"id": "Bard",
-			"displayName": "Bard"
-		}
+			id: "Bard",
+			displayName: "Bard"
+		},
 		"82": {
-			"id": "Mordekaiser",
-			"displayName": "Mordekaiser"
-		}
+			id: "Mordekaiser",
+			displayName: "Mordekaiser"
+		},
 		"245": {
-			"id": "Ekko",
-			"displayName": "Ekko"
-		}
+			id: "Ekko",
+			displayName: "Ekko"
+		},
 		"83": {
-			"id": "Yorick",
-			"displayName": "Yorick"
-		}
+			id: "Yorick",
+			displayName: "Yorick"
+		},
 		"80": {
-			"id": "Pantheon",
-			"displayName": "Pantheon"
-		}
+			id: "Pantheon",
+			displayName: "Pantheon"
+		},
 		"81": {
-			"id": "Ezreal",
-			"displayName": "Ezreal"
-		}
+			id: "Ezreal",
+			displayName: "Ezreal"
+		},
 		"86": {
-			"id": "Garen",
-			"displayName": "Garen"
-		}
+			id: "Garen",
+			displayName: "Garen"
+		},
 		"84": {
-			"id": "Akali",
-			"displayName": "Akali"
-		}
+			id: "Akali",
+			displayName: "Akali"
+		},
 		"85": {
-			"id": "Kennen",
-			"displayName": "Kennen"
-		}
+			id: "Kennen",
+			displayName: "Kennen"
+		},
 		"67": {
-			"id": "Vayne",
-			"displayName": "Vayne"
-		}
+			id: "Vayne",
+			displayName: "Vayne"
+		},
 		"126": {
-			"id": "Jayce",
-			"displayName": "Jayce"
-		}
+			id: "Jayce",
+			displayName: "Jayce"
+		},
 		"127": {
-			"id": "Lissandra",
-			"displayName": "Lissandra"
-		}
+			id: "Lissandra",
+			displayName: "Lissandra"
+		},
 		"69": {
-			"id": "Cassiopeia",
-			"displayName": "Cassiopeia"
-		}
+			id: "Cassiopeia",
+			displayName: "Cassiopeia"
+		},
 		"68": {
-			"id": "Rumble",
-			"displayName": "Rumble"
-		}
+			id: "Rumble",
+			displayName: "Rumble"
+		},
 		"121": {
-			"id": "Khazix",
-			"displayName": "Khazix"
-		}
+			id: "Khazix",
+			displayName: "Kha'Zix"
+		},
 		"122": {
-			"id": "Darius",
-			"displayName": "Darius"
-		}
+			id: "Darius",
+			displayName: "Darius"
+		},
 		"120": {
-			"id": "Hecarim",
-			"displayName": "Hecarim"
-		}
+			id: "Hecarim",
+			displayName: "Hecarim"
+		},
 		"72": {
-			"id": "Skarner",
-			"displayName": "Skarner"
-		}
+			id: "Skarner",
+			displayName: "Skarner"
+		},
 		"236": {
-			"id": "Lucian",
-			"displayName": "Lucian"
-		}
+			id: "Lucian",
+			displayName: "Lucian"
+		},
 		"74": {
-			"id": "Heimerdinger",
-			"displayName": "Heimerdinger"
-		}
+			id: "Heimerdinger",
+			displayName: "Heimerdinger"
+		},
 		"75": {
-			"id": "Nasus",
-			"displayName": "Nasus"
-		}
+			id: "Nasus",
+			displayName: "Nasus"
+		},
 		"238": {
-			"id": "Zed",
-			"displayName": "Zed"
-		}
+			id: "Zed",
+			displayName: "Zed"
+		},
 		"76": {
-			"id": "Nidalee",
-			"displayName": "Nidalee"
-		}
+			id: "Nidalee",
+			displayName: "Nidalee"
+		},
 		"134": {
-			"id": "Syndra",
-			"displayName": "Syndra"
-		}
+			id: "Syndra",
+			displayName: "Syndra"
+		},
 		"59": {
-			"id": "JarvanIV",
-			"displayName": "JarvanIV"
-		}
+			id: "JarvanIV",
+			displayName: "Jarvan IV"
+		},
 		"133": {
-			"id": "Quinn",
-			"displayName": "Quinn"
-		}
+			id: "Quinn",
+			displayName: "Quinn"
+		},
 		"58": {
-			"id": "Renekton",
-			"displayName": "Renekton"
-		}
+			id: "Renekton",
+			displayName: "Renekton"
+		},
 		"57": {
-			"id": "Maokai",
-			"displayName": "Maokai"
-		}
+			id: "Maokai",
+			displayName: "Maokai"
+		},
 		"136": {
-			"id": "AurelionSol",
-			"displayName": "AurelionSol"
-		}
+			id: "AurelionSol",
+			displayName: "Aurelion Sol"
+		},
 		"56": {
-			"id": "Nocturne",
-			"displayName": "Nocturne"
-		}
+			id: "Nocturne",
+			displayName: "Nocturne"
+		},
 		"55": {
-			"id": "Katarina",
-			"displayName": "Katarina"
-		}
+			id: "Katarina",
+			displayName: "Katarina"
+		},
 		"64": {
-			"id": "LeeSin",
-			"displayName": "LeeSin"
-		}
+			id: "LeeSin",
+			displayName: "Lee Sin"
+		},
 		"62": {
-			"id": "MonkeyKing",
-			"displayName": "MonkeyKing"
-		}
+			id: "MonkeyKing",
+			displayName: "Wukong"
+		},
 		"268": {
-			"id": "Azir",
-			"displayName": "Azir"
-		}
+			id: "Azir",
+			displayName: "Azir"
+		},
 		"63": {
-			"id": "Brand",
-			"displayName": "Brand"
-		}
+			id: "Brand",
+			displayName: "Brand"
+		},
 		"131": {
-			"id": "Diana",
-			"displayName": "Diana"
-		}
+			id: "Diana",
+			displayName: "Diana"
+		},
 		"60": {
-			"id": "Elise",
-			"displayName": "Elise"
-		}
+			id: "Elise",
+			displayName: "Elise"
+		},
 		"267": {
-			"id": "Nami",
-			"displayName": "Nami"
-		}
+			id: "Nami",
+			displayName: "Nami"
+		},
 		"266": {
-			"id": "Aatrox",
-			"displayName": "Aatrox"
-		}
+			id: "Aatrox",
+			displayName: "Aatrox"
+		},
 		"61": {
-			"id": "Orianna",
-			"displayName": "Orianna"
-		}
+			id: "Orianna",
+			displayName: "Orianna"
+		},
 		"143": {
-			"id": "Zyra",
-			"displayName": "Zyra"
-		}
+			id: "Zyra",
+			displayName: "Zyra"
+		},
 		"48": {
-			"id": "Trundle",
-			"displayName": "Trundle"
-		}
+			id: "Trundle",
+			displayName: "Trundle"
+		},
 		"45": {
-			"id": "Veigar",
-			"displayName": "Veigar"
-		}
+			id: "Veigar",
+			displayName: "Veigar"
+		},
 		"44": {
-			"id": "Taric",
-			"displayName": "Taric"
-		}
+			id: "Taric",
+			displayName: "Taric"
+		},
 		"51": {
-			"id": "Caitlyn",
-			"displayName": "Caitlyn"
-		}
+			id: "Caitlyn",
+			displayName: "Caitlyn"
+		},
 		"53": {
-			"id": "Blitzcrank",
-			"displayName": "Blitzcrank"
-		}
+			id: "Blitzcrank",
+			displayName: "Blitzcrank"
+		},
 		"54": {
-			"id": "Malphite",
-			"displayName": "Malphite"
-		}
+			id: "Malphite",
+			displayName: "Malphite"
+		},
 		"254": {
-			"id": "Vi",
-			"displayName": "Vi"
-		}
+			id: "Vi",
+			displayName: "Vi"
+		},
 		"50": {
-			"id": "Swain",
-			"displayName": "Swain"
+			id: "Swain",
+			displayName: "Swain"
 		}
 	}
 });
