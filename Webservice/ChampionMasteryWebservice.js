cf169811f0f766e2d62ce4d51db0a30ab0cd36d5
Frederic Birke <f.birke@yahoo.de>
2016-05-05 12:07:28 +0200

    added function to determine users default platform by coordinates

--- Webservice/ChampionMasteryWebservice.js
+++ Webservice/ChampionMasteryWebservice.js
@@ -5,16 +5,22 @@ var Promise = require('bluebird');
 var http = require('http');
 var $ = require('./node_modules/jquery/dist/jquery.min.js')
 var rp = require('request-promise');
+var cors = require('cors');
+var jsonQuery = require('json-query');
+var dbAccess = require('./MariaDBService.js');
 
-var apikey;
+
+var riotApiKey;
+var googleApiKey;
 
 
 //------------------------------------------------------
 // Event that gets fired on service launch
 // and can be used to run initializing steps for the service
 //------------------------------------------------------
+app.use(cors());
 app.on('listening', function () {
-  // Aquire the API Key and save it to the apikey variable
+  // Aquire the API Key and save it to the riotApiKey variable
 
 });
 
@@ -23,8 +29,9 @@ app.on('listening', function () {
 //------------------------------------------------------
 app.listen(3002, function() {
   console.log('Listening on port 3002!');
-  apikey = require('./apiKey.json').key;
-  console.log(apikey);
+  riotApiKey = require('./riotApiKey.json').key;
+  googleApiKey = require('./googleApiKey.json').key;
+  console.log(riotApiKey);
 });
 
 //------------------------------------------------------
@@ -34,10 +41,18 @@ app.listen(3002, function() {
 //------------------------------------------------------
 function createRiotApiHttpRequest(subrequest){
   var riotApiRequest = {
-    uri:'https://euw.api.pvp.net' +subrequest + '?' + apikey,
+    uri:'https://euw.api.pvp.net' +subrequest + '?' + riotApiKey,
     json: true
   }
   return riotApiRequest;
+}
+
+function createGoogleApiHttpRequest(subrequest){
+  var googleAPIRequest = {
+    uri:'https://maps.googleapis.com/' +subrequest + '&' + googleApiKey,
+    json: true
+  }
+  return googleAPIRequest;
 }
 
 
@@ -46,7 +61,7 @@ function createRiotApiHttpRequest(subrequest){
 //-------------------------------------------clear-----------
 function GetSummonerId(summonerName, platform){
     return new Promise(function(resolve, reject){
-      var requestOptions = createRiotApiHttpRequest('/api/lol/'+ platform +'/v1.4/summoner/by-name/' + summonerName);
+      var requestOptions = createRiotApiHttpRequest('/api/lol/'+ platform +'/v1.4/summoner/by-name/' + summonerName + '&result_type=country');
       rp(requestOptions).then(function(response){
         resolve((response[summonerName.toString()].id));
       }, function(error){
@@ -54,6 +69,43 @@ function GetSummonerId(summonerName, platform){
         console.log(error);
       });
     });
+  }
+
+  //-----------------------------------------------------
+  // getter function "http://domain:3000/GetAllChampionMasteries/p/:platform/u/:userId"
+  // req.params:  p(String) platform id.
+  //              u(String) player ID
+  //------------------------------------------------------
+  app.get('/GetSummonerContinent/la/:latitude/lo/:longitude', function(req,res) {
+    var latitude = req.params.latitude;
+    var longitude = req.params.longitude;
+    console.log(latitude);
+    console.log(longitude);
+    var googleAPIRequestOptions = createGoogleApiHttpRequest ('maps/api/geocode/json?latlng=' + latitude + ',' + longitude);
+    console.log(googleAPIRequestOptions);
+    rp(googleAPIRequestOptions).then(function(response){
+      var countryCode = searchByKey(response.results[0].address_components, 'country');
+      console.log(countryCode);
+      dbAccess.getLolContinentCodeLocalFromDB(countryCode,
+        function (resp){
+          console.log(resp);
+          res.send(resp);
+        }
+      );
+    }, function(error){
+      console.log('Error: GetSummonerContinent!!!!');
+      console.log(error);
+    });
+
+  });
+
+  function searchByKey(array, key) {
+    for (var i = 0, l = array.length; i < l; i++){
+      if (array[i]['types'][0] === key) {
+        return array[i]['short_name'];
+      }
+    }
+    return false;
   }
 
 
