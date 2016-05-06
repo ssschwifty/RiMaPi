3baa36c3785f5324af4da9be6648132e6b420f37
Sarah Wöckener <ssschwifty@outlook.com>
2016-05-07 00:59:24 +0200

    add level up view + logic, sorting, stuff

--- Angular-Project/app/scripts/controller/ui/levelController.js
+++ Angular-Project/app/scripts/controller/ui/levelController.js
@@ -1,15 +1,41 @@
 angular.module('riot.controller.ui')
-.controller('LevelController', function($scope, SharedProperties) {
+.controller('LevelController', function($scope, SharedProperties, UserData, Sort) {
+
+	$scope.levelCap = [
+		{cap: 0},
+		{cap: 1800},
+		{cap: 6000},
+		{cap: 12600},
+		{cap: 21600}
+	];
+
+	$('html').on('region:change', function(region) {
+		setTimeout(function() {
+			getData();
+		}, 20);
+	});
+	$('html').on('summoner:change', function() {
+		getData();
+	});
+	$scope.$on('$stateChangeSuccess', function() {
+		getData();
+	});
 
 	function getData() {
 		if(UserData.regionId != undefined && UserData.summoner != undefined && UserData.summoner != "") {
 			SharedProperties.getAllChampionMasteries(UserData.regionId, UserData.summoner)
 			.then(function(response) {
+				$scope.champions = [];
 				var testresult = response.data;
 				for (var i = 0; i < testresult.length; i++) {
-					console.log(testresult[i]);
+					testresult[i].nameId = SharedProperties.getChampionNameIdById(testresult[i].championId);
 					testresult[i].displayName = SharedProperties.getChampionDisplayNameById(testresult[i].championId);
+					if(testresult[i].championPointsUntilNextLevel > 0) {
+						$scope.champions.push(testresult[i]);
+						console.log(testresult[i]);
+					}
 				}
+				Sort.sortByPointsUntilNextLevel($scope.champions);
 			});
 		}
 	}
--- Angular-Project/app/scripts/services/sortService.js
+++ Angular-Project/app/scripts/services/sortService.js
@@ -10,17 +10,27 @@ angular.module('riot.services')
 		"N\\A"
 	];
 
-	sortFunction = function(a, b) {
+	sortFunctionGradeChampionPoints = function(a, b) {
 		if(gradeOrder.indexOf(a.highestGrade) - gradeOrder.indexOf(b.highestGrade) == 0) {
 			return b.championPoints - a.championPoints;
 		} else {
 			return gradeOrder.indexOf(a.highestGrade) - gradeOrder.indexOf(b.highestGrade);
 		}
+	};
+	sortFunctionPointUntilNextLevel = function(a, b) {
+		return a.championPointsUntilNextLevel - b.championPointsUntilNextLevel;
 	}
 	
 	return {
 		sortByGradeThenChampionPoints: function(champions) {
-			champions.sort(sortFunction);
+			champions.sort(sortFunctionGradeChampionPoints);
+			var results = champions.map(function(a) {
+				[a.championId];
+			});
+			return results;
+		},
+		sortByPointsUntilNextLevel: function(champions){
+			champions.sort(sortFunctionPointUntilNextLevel);
 			var results = champions.map(function(a) {
 				[a.championId];
 			});
