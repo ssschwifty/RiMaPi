29871758091e2169717ff202b7dba92a2bbc28d1
Sarah Wöckener <ssschwifty@outlook.com>
2016-05-06 22:55:27 +0200

    remove constants

--- Angular-Project/app/scripts/constants/stringConstants.js
+++ Angular-Project/app/scripts/constants/stringConstants.js
@@ -1,10 +1,6 @@
 angular.module('riot')
 .constant('StringConstants', {
 
-	"btnGrade": 'Grade',
-	"btnMastery": 'Mystery Chests',
-	"btnLevel": 'Level Up!',
-	"btnCompare" :"Compare Yourself",
 	"btnDocu": 'Documentation',
 	"btnProceedAnyway": "Proceed and enter information later",
 	"welcomeText": "Welcome, Summoner!",
--- Angular-Project/app/scripts/controller/mainController.js
+++ Angular-Project/app/scripts/controller/mainController.js
@@ -2,10 +2,6 @@ angular.module('riot.controller')
 .controller('MainController', function($scope, StringConstants) {
 
 	function initiate() {
-		$scope.btnGrade = StringConstants.btnGrade;
-		$scope.btnMastery = StringConstants.btnMastery;
-		$scope.btnLevel = StringConstants.btnLevel;
-		$scope.btnCompare = StringConstants.btnCompare;
 		$scope.btnDocu = StringConstants.btnDocu;
 		$scope.btnProceedAnyway = StringConstants.btnProceedAnyway;
 		$scope.btnEnter = StringConstants.btnEnter;
