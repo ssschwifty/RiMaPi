3e0d9c8e21ca2b3b330204bb761a97ba2fde316d
Sarah Wöckener <ssschwifty@outlook.com>
2016-05-08 15:43:43 +0200

    style popup

--- Angular-Project/app/scripts/controller/mainController.js
+++ Angular-Project/app/scripts/controller/mainController.js
@@ -6,7 +6,7 @@ angular.module('riot.controller')
 		$scope.btnProceedAnyway = "Proceed and enter information later";
 		$scope.btnEnter = "Enter";
 		$scope.welcomeText = "Welcome, Summoner!";
-		$scope.requestsExceededMessage = "We are very sorry, but our API request limit is exceeded. :( Please wait 10 seconds and try again! :)";
+		$scope.requestsExceededMessage = "request limit";
 		$scope.summonerNotFound = "The name you entered could not be resolved to a summoner in the selected region.";
 	}
 	
