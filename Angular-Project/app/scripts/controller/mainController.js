dc2c756e6a88abcb10e91caa18a9feda6daf5782
Sarah Wöckener <ssschwifty@outlook.com>
2016-05-08 15:23:06 +0200

    removed stringConstants.js and initialize texts in mainController

--- Angular-Project/app/scripts/controller/mainController.js
+++ Angular-Project/app/scripts/controller/mainController.js
@@ -1,11 +1,13 @@
 angular.module('riot.controller')
-.controller('MainController', function($scope, StringConstants) {
+.controller('MainController', function($scope, $uibModal) {
 
 	function initiate() {
-		$scope.btnDocu = StringConstants.btnDocu;
-		$scope.btnProceedAnyway = StringConstants.btnProceedAnyway;
-		$scope.btnEnter = StringConstants.btnEnter;
-		$scope.welcomeText = StringConstants.welcomeText;
+		$scope.btnDocu = "Documentation";
+		$scope.btnProceedAnyway = "Proceed and enter information later";
+		$scope.btnEnter = "Enter";
+		$scope.welcomeText = "Welcome, Summoner!";
+		$scope.requestsExceededMessage = "We are very sorry, but our API request limit is exceeded. :( Please wait 10 seconds and try again! :)";
+		$scope.summonerNotFound = "The name you entered could not be resolved to a summoner in the selected region.";
 	}
 	
 	initiate();
