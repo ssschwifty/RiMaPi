1080e9ecc2fdace8988579adfeb6609831f10e5e
Frederic Birke <f.birke@yahoo.de>
2016-05-10 00:32:51 +0200

    added awesome new feature: You can now press enter to enter the website :)

--- Angular-Project/app/scripts/controller/ui/splashscreenController.js
+++ Angular-Project/app/scripts/controller/ui/splashscreenController.js
@@ -1,9 +1,6 @@
 angular.module('riot.controller.ui')
 .controller('SplashscreenController', function($scope, $state) {
-	$('#summonerInput').on('keypress', function(e) {
-		if (e.keyCode == 13) {
-			$state.go('page.home');
-			$('#compareSummonerInput').blur();
-		}
+	$('html').on('summoner:change', function() {
+		$state.go("page.home");
 	});
 });
