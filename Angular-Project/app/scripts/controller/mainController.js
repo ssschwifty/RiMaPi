16cdfc646b5fb2e3abcb2886a9f4d85f5a413d3b
Sarah Wöckener <ssschwifty@outlook.com>
2016-05-10 01:05:13 +0200

    everything is dirtyyyyyy

--- Angular-Project/app/scripts/controller/mainController.js
+++ Angular-Project/app/scripts/controller/mainController.js
@@ -4,6 +4,8 @@
 angular.module('riot.controller')
 .controller('MainController', function($scope, $uibModal, $location, $state, UserData) {
 
+	
+
 	function initialize() {
 		$scope.btnDocu = "Documentation";
 		$scope.btnProceedAnyway = "Proceed and enter information later";
@@ -15,46 +17,44 @@ angular.module('riot.controller')
 		$scope.summonerNotFound = "Oops! The name you entered could not be resolved to a summoner in the selected region. Please check if you have spelled your summonername correctly and if you have selected the correct region! Then try again!";
 		$scope.enterSummonerNames = "Whoa... slow down, my friend! You should enter the names of two summoners instead of sending empty Emails.";
 		$scope.enterEmail = "Please enter a Email address";
-
-		$scope.headerButtonsArray = {
-			home: {name: 'home', text: 'Home'},
-			improve: {name: 'improve', text: 'Improve yourself!'},
-			loot: {name: 'loot', text: 'Mystery Chests'},
-			level: {name: 'level', text: 'Level Up!'},
-			compare: {name: 'compare', text: 'Compare Yourself'}
-		};
-
-		var hash = $location.search();
-		var url = $location.path().replace(/\//g, '');
-
-		if(hash != undefined) {
-			if(url == "entry") {
-				$state.go('splashScreen'); 
-			} else {
-				$state.go('page.'+url, {as: hash["as"], ar: hash["ar"], ba: hash["ba"], br: hash["br"]});
-			}
-		} else {
-			$state.go('splashScreen');
-		}
-
-		// get summoner names from url if they are undefined until now
-		if (UserData.summoner == undefined) {
-			UserData.summoner = $location.search()["as"];
-			UserData.regionId = $location.search()["ar"];
-		}
-		if (UserData.compareSummoner == undefined) {
-			UserData.compareSummoner = $location.search()["bs"];
-			UserData.compareRegionId = $location.search()["br"];
-		}
 	}
 	initialize();
 
+	$scope.headerButtonsArray = {
+				home: {name: 'home', text: 'Home'},
+				improve: {name: 'improve', text: 'Improve yourself!'},
+				loot: {name: 'loot', text: 'Mystery Chests'},
+				level: {name: 'level', text: 'Level Up!'},
+				compare: {name: 'compare', text: 'Compare Yourself'}
+			};
+
 	$scope.setActivePage = function(index) {
-		for (var i = 0; i < $scope.headerButtonsArray.length; i++) {
-			$scope.headerButtonsArray[i].active = false;
+		if(index != "entry") {
+			for(var tab in $scope.headerButtonsArray) {
+				$scope.headerButtonsArray[tab].active = false;
+			}
+			$scope.headerButtonsArray[index].active = true;
 		}
-		$scope.headerButtonsArray[index].active = true;
 	}
+	var hash = $location.search();
+	var url = $location.path().replace(/\//g, '');
+	if(url == "entry") {
+		$state.go('splashScreen'); 
+	} else {
+		$scope.setActivePage(url);
+		$state.go('page.'+url, {as: hash["as"], ar: hash["ar"], ba: hash["ba"], br: hash["br"]});
+	}
+
+	// get summoner names from url if they are undefined until now
+	if (UserData.summoner == undefined) {
+		UserData.summoner = $location.search()["as"];
+		UserData.regionId = $location.search()["ar"];
+	}
+	if (UserData.compareSummoner == undefined) {
+		UserData.compareSummoner = $location.search()["bs"];
+		UserData.compareRegionId = $location.search()["br"];
+	}
+
 	$scope.openPopup = function(popUpParameters) {
 		var popup = $uibModal.open({
 			templateUrl: './views/popupView.html',
--- Angular-Project/app/scripts/controller/ui/splashscreenController.js
+++ Angular-Project/app/scripts/controller/ui/splashscreenController.js
@@ -1,6 +1,7 @@
 angular.module('riot.controller.ui')
 .controller('SplashscreenController', function($scope, $state) {
 	$('html').on('summoner:change', function() {
+		$scope.setActivePage('home');
 		$state.go("page.home");
 	});
 });
